e154d2bee2975944efa0a440e7dfbaa4be44dcf1
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2022-10-14 17:06:22 +0700

    Add files via upload

---
+++ local.py
@@ -0,0 +1,8 @@
+import requests
+
+res = requests.put("http://127.0.0.1:3000/api/courses/1", {"name": "Golang", "videos": 5})
+# get - получение
+# delete - удаление
+# put - обновление
+# port - добавление
+print(res.json())
